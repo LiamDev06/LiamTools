7b298157357fdb2de56cbbd4062848c77688c489
LiamDev06 <liam@hillefors.se>
2024-02-08 01:19:55 +0100

    Update ItemUtil.java

--- core/src/main/java/com/github/liamdev06/item/util/ItemUtil.java
+++ core/src/main/java/com/github/liamdev06/item/util/ItemUtil.java
@@ -2,6 +2,7 @@ package com.github.liamdev06.item.util;
 
 import io.leangen.geantyref.TypeToken;
 import lombok.experimental.UtilityClass;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -15,6 +16,11 @@ public class ItemUtil {
      * Configuration type token for {@link ItemStack} serialization.
      */
     public static final TypeToken<ItemStack> ITEM_TYPE_TOKEN = TypeToken.get(ItemStack.class);
+
+    /**
+     * Empty ItemStack with air.
+     */
+    public static final ItemStack AIR = new ItemStack(Material.AIR);
 
     /**
      * Strips the visual metadata from an ItemStack like its display name and lore.
